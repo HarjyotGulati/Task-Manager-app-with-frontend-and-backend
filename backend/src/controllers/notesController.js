2325cec0d93645c28007b5343d1782ec5d2dbdc6
Harjyot Gulati <harjyotsinghgulati@gmail.com>
2025-06-14 23:46:59 +0530

    backend setup completed

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,6 +1,6 @@
 import Note from "../model/Note.js"
 
-export async function getAllNotes(req,res){
+export async function getAllNotes(_,res){
     try{
         const notes = await Note.find().sort({ createdAt: -1 }); // -1 will sort in desc. order (newest first)
         res.status(200).json(notes);
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,23 @@ import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
+import rateLimiter from "./middlewear/rateLimiter.js";
+
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json())
+app.use(rateLimiter);
 app.use('/api/notes',notesRoutes);
 
-connectDB();
 
-app.get("/",(req,res)=>{
-    res.send("hello");
-})
-
-
-app.listen(port,()=>{
+connectDB().then(()=>{
+    app.listen(port,()=>{
     console.log(`server is now listening at port:${port}`);
 })
+});
+
+
+
